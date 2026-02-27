086d4bdb4b1b3cf339ffa6e42145fc9bcf673b0a
artemkliuienkov <artemkluenkov41@gmail.com>
2026-02-27 13:15:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(per["name"], per["age"]) for per in people]
+    for index, per in enumerate(people):
+        if per.get("wife"):
+            person_list[index].wife = Person.people[per["wife"]]
+        elif per.get("husband"):
+            person_list[index].husband = Person.people[per["husband"]]
+    return person_list
